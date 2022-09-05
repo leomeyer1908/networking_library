4f6bb07e23115bd1be42ee9d671cbe2de62185e2
Leo Meyer <leomeyer1908@users.noreply.github.com>
2022-09-05 00:27:53 -0500

    finished up the part that gets request_uri and also added a decoder for escape codes

--- HTTP_Server.cpp
+++ HTTP_Server.cpp
@@ -1,5 +1,55 @@
 #include "HTTP_Server.hpp"
 
+void decode_uri(char* uri, int uri_size, char* decoded) {
+    const int ESCAPE_CODES_NUM = 23;
+    struct escape_code {
+        char hex_code[2];
+        char character;
+    };
+    struct escape_code escape_codes[ESCAPE_CODES_NUM] = {
+        {{'2', '0'}, ' '},
+        {{'3', 'C'}, '<'},
+        {{'3', 'E'}, '>'},
+        {{'2', '3'}, '#'},
+        {{'2', '5'}, '%'},
+        {{'2', 'B'}, '+'},
+        {{'7', 'B'}, '{'},
+        {{'7', 'D'}, '}'},
+        {{'7', 'C'}, '|'},
+        {{'5', 'C'}, '\\'},
+        {{'5', 'E'}, '^'},
+        {{'7', 'E'}, '~'},
+        {{'5', 'B'}, '['},
+        {{'5', 'D'}, ']'},
+        {{'6', '0'}, '`'},
+        {{'3', 'B'}, ';'},
+        {{'2', 'F'}, '/'},
+        {{'3', 'F'}, '?'},
+        {{'3', 'A'}, ':'},
+        {{'4', '0'}, '@'},
+        {{'3', 'D'}, '='},
+        {{'2', '6'}, '&'},
+        {{'2', '4'}, '$'}
+    };
+    int decoded_index = 0;
+    for (int i = 0; i < uri_size; i++) {
+        if (uri[i] == '%') {
+            for (int j = 0; j < ESCAPE_CODES_NUM; j++) {
+                if (escape_codes[j].hex_code[0] == uri[i+1]) {
+                    if (escape_codes[j].hex_code[1] == uri[i+2]) {
+                        decoded[decoded_index] = escape_codes[j].character;
+                        i += 2;
+                    } 
+                }
+            }
+        }
+        else {
+            decoded[decoded_index] = uri[i];
+        }
+        decoded_index++;
+    }
+}
+
 HTTP_Server::HTTP_Server(const char *port, char* html, int html_size) {
     int poll_count;
     std::vector<pollfd> poll_fds;
@@ -41,7 +91,7 @@ HTTP_Server::HTTP_Server(const char *port, char* html, int html_size) {
                     /*METHOD*/
                     char* request_method = buf;
                     int request_method_size = 0;
-                    for (char* ptr; ptr < buf + sizeof(buf); ptr++) {
+                    for (char* ptr = buf; ptr < buf + sizeof(buf); ptr++) {
                         if (*ptr == ' ') {
                             break;
                         }
@@ -67,7 +117,8 @@ HTTP_Server::HTTP_Server(const char *port, char* html, int html_size) {
                     /*REQUEST-URI*/
                     char* request_uri = (buf+request_method_size+1);
                     int request_uri_size = 0;
-                    for (char* ptr; ptr < buf + sizeof(buf); ptr++) {
+                    int escape_codes_num = 0;
+                    for (char* ptr = request_uri; ptr < buf + sizeof(buf); ptr++) {
                         if (*ptr == ' ') {
                             break;
                         }
@@ -82,9 +133,25 @@ HTTP_Server::HTTP_Server(const char *port, char* html, int html_size) {
                             }
                             //INVALID REQUEST, DO SOMETHING HERE
                         }
+                        if (*ptr == '%') {
+                            escape_codes_num++;
+                        }
                         request_uri_size++;
                     }
+                    //CHECK IF % WITH INVALID CODES CAN GIVE ERROR
+                    int decoded_uri_size = request_uri_size - escape_codes_num*2;
+                    if (decoded_uri_size == 0) {
+                        decoded_uri_size = 1;
+                    }
+                    char decoded_uri[decoded_uri_size];
+                    if (decoded_uri_size == 1) {
+                        decoded_uri[0] = '/';
+                    }
+                    decode_uri(request_uri, request_uri_size, decoded_uri);
+
                     /*HTTP-VERSION*/
+                    
+
 
 
 
@@ -93,7 +160,7 @@ HTTP_Server::HTTP_Server(const char *port, char* html, int html_size) {
 
 
                     //print request to console
-                    for (char* ptr; ptr < buf + sizeof(buf); ptr++) {
+                    for (char* ptr = buf; ptr < buf + sizeof(buf); ptr++) {
                         std::cout << *ptr;
                     }
                     std::cout << std::endl;
